6c34d67195e997e1286bbac22b7cad525d9511bf
bee <rkddbtjd123@gmail.com>
2022-07-06 00:04:12 +0900

    build: add reactor module to java project

---
+++ reactor/src/test/java/me/java/reactor/flux/FluxTest.java
@@ -0,0 +1,12 @@
+package me.java.reactor.flux;
+
+import org.junit.jupiter.api.Test;
+
+public class FluxTest {
+
+    @Test
+    void test() {
+        System.out.println("hello world");
+    }
+
+}
