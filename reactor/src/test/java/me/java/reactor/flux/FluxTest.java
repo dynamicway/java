784b2b5cc049731890d0e3b0466c234d5979ddea
bee <rkddbtjd123@gmail.com>
2022-07-06 01:02:41 +0900

    test: add FluxTest::subscribe

--- reactor/src/test/java/me/java/reactor/flux/FluxTest.java
+++ reactor/src/test/java/me/java/reactor/flux/FluxTest.java
@@ -1,12 +1,21 @@
 package me.java.reactor.flux;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FluxTest {
 
     @Test
-    void test() {
-        System.out.println("hello world");
+    void subscribe() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Integer[] integers = {1, 2, 3};
+        Flux.just(integers)
+                .subscribe(list::add);
+        assertThat(list).containsExactly(integers);
     }
 
 }
