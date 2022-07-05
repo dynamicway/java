ba47be75476752fec58bc2bb632b9ed158e2ed00
bee <rkddbtjd123@gmail.com>
2022-07-06 01:35:22 +0900

    test: add FluxTest::when_subscribe_calls_onNext_then_executed_doOnNext

--- reactor/src/test/java/me/java/reactor/flux/FluxTest.java
+++ reactor/src/test/java/me/java/reactor/flux/FluxTest.java
@@ -1,11 +1,16 @@
 package me.java.reactor.flux;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 public class FluxTest {
 
@@ -15,7 +20,27 @@ public class FluxTest {
         Integer[] integers = {1, 2, 3};
         Flux.just(integers)
                 .subscribe(list::add);
+
         assertThat(list).containsExactly(integers);
+    }
+
+    @Test
+    void when_subscribe_calls_onNext_then_executed_doOnNext() {
+        List<String> list = new ArrayList<>();
+        Integer[] integers = {1, 2, 3};
+        Flux<Integer> flux = Flux.just(integers)
+                .doOnNext(integer -> list.add("doOnNext" + integer));
+
+        flux.subscribe(integer -> list.add("subscribe" + integer));
+
+        assertThat(list).containsExactly(
+                "doOnNext" + 1,
+                "subscribe" + 1,
+                "doOnNext" + 2,
+                "subscribe" + 2,
+                "doOnNext" + 3,
+                "subscribe" + 3
+        );
     }
 
 }
