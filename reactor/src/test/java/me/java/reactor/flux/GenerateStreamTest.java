e14d7817950890355be1fd286c70ce664b77eff2
bee <rkddbtjd123@gmail.com>
2022-07-06 17:13:51 +0900

    test: Add GenerateStreamTest::range_in_Flux

--- reactor/src/test/java/me/java/reactor/flux/GenerateStreamTest.java
+++ reactor/src/test/java/me/java/reactor/flux/GenerateStreamTest.java
@@ -1,8 +1,16 @@
 package me.java.reactor.flux;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class GenerateStreamTest {
@@ -17,6 +25,22 @@ public class GenerateStreamTest {
     void just_throws_NullPointerException_when_argument_is_null() {
         assertThatCode(() -> Mono.just(null))
                 .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    void range_in_Flux() {
+        List<Integer> list = new ArrayList<>();
+
+        int start = 0;
+        int count = 100;
+        Flux.range(start, count)
+                .subscribe(list::add);
+
+        List<Integer> result = IntStream.range(start, count)
+                .boxed()
+                .toList();
+
+        assertThat(list).containsExactlyElementsOf(result);
     }
 
 
