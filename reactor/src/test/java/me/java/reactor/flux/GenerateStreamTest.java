f4f46ef06f85fb90d03841d4fd5f644ea6e49784
bee <rkddbtjd123@gmail.com>
2022-07-06 17:49:42 +0900

    test: Add GenerateStreamTest::generate_in_Flux

--- reactor/src/test/java/me/java/reactor/flux/GenerateStreamTest.java
+++ reactor/src/test/java/me/java/reactor/flux/GenerateStreamTest.java
@@ -3,10 +3,12 @@ package me.java.reactor.flux;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SynchronousSink;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -39,6 +41,31 @@ public class GenerateStreamTest {
         List<Integer> result = IntStream.range(start, count)
                 .boxed()
                 .toList();
+
+        assertThat(list).containsExactlyElementsOf(result);
+    }
+
+    @Test
+    void generate_in_Flux() {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Consumer<SynchronousSink<Integer>> synchronousSinkConsumer = new Consumer<>() {
+
+            private int i = 0;
+
+            @Override
+            public void accept(SynchronousSink<Integer> integerSynchronousSink) {
+                integerSynchronousSink.next(i++);
+                if (i == 10)
+                    integerSynchronousSink.complete();
+            }
+
+        };
+
+        Flux.generate(synchronousSinkConsumer).subscribe(list::add);
+
+        List<Integer> result = IntStream.range(0, 10).boxed().toList();
 
         assertThat(list).containsExactlyElementsOf(result);
     }
