ecc3f075812d9f73b8712190f052853811877002
bee <rkddbtjd123@gmail.com>
2022-09-16 16:56:26 +0900

    test: Add CreateFluxTest::create_Flux_by_just

--- reactor/src/test/java/me/java/reactor/flux/CreateFluxTest.java
+++ reactor/src/test/java/me/java/reactor/flux/CreateFluxTest.java
@@ -2,15 +2,9 @@ package me.java.reactor.flux;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.SynchronousSink;
 import reactor.test.StepVerifier;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.IntStream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class CreateFluxTest {
 
@@ -20,9 +14,7 @@ class CreateFluxTest {
         int until = 100;
         Flux<Integer> givenFluxByRange = Flux.range(since, until);
 
-        Integer[] expectedDataOfFlux = IntStream.range(since, until)
-                .boxed()
-                .toArray(Integer[]::new);
+        Integer[] expectedDataOfFlux = IntStream.range(since, until).boxed().toArray(Integer[]::new);
 
         StepVerifier.create(givenFluxByRange)
                 .expectNext(expectedDataOfFlux)
@@ -30,28 +22,13 @@ class CreateFluxTest {
     }
 
     @Test
-    void create_Flux_by_generate() {
+    void create_Flux_by_just() {
+        Integer[] data = {1, 2, 3};
+        Flux<Integer> givenFluxByJust = Flux.just(data);
 
-        ArrayList<Integer> list = new ArrayList<>();
-
-        Consumer<SynchronousSink<Integer>> synchronousSinkConsumer = new Consumer<>() {
-
-            private int i = 0;
-
-            @Override
-            public void accept(SynchronousSink<Integer> integerSynchronousSink) {
-                integerSynchronousSink.next(i++);
-                if (i == 10)
-                    integerSynchronousSink.complete();
-            }
-
-        };
-
-        Flux.generate(synchronousSinkConsumer).subscribe(list::add);
-
-        List<Integer> result = IntStream.range(0, 10).boxed().toList();
-
-        assertThat(list).containsExactlyElementsOf(result);
+        StepVerifier.create(givenFluxByJust)
+                .expectNext(data)
+                .verifyComplete();
     }
 
 }
