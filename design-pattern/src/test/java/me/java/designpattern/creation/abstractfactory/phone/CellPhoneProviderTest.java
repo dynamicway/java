ffc9b84dace1815e605805a17c3397bbb04626ab
bee <rkddbtjd123@gmail.com>
2022-09-19 16:07:29 +0900

    feat: Change the CellPhoneProvider constructor to depend on CellPhoneFactories

--- design-pattern/src/test/java/me/java/designpattern/creation/abstractfactory/phone/CellPhoneProviderTest.java
+++ design-pattern/src/test/java/me/java/designpattern/creation/abstractfactory/phone/CellPhoneProviderTest.java
@@ -1,7 +1,11 @@
 package me.java.designpattern.creation.abstractfactory.phone;
 
+import me.java.designpattern.creation.abstractfactory.phone.apple.AppleCellPhoneFactory;
+import me.java.designpattern.creation.abstractfactory.phone.samsung.SamsungCellPhoneFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,7 +14,7 @@ class CellPhoneProviderTest {
     @ParameterizedTest
     @EnumSource(CellPhone.Type.class)
     void the_cell_phone_type_is_the_same_as_requested(CellPhone.Type cellPhoneType) {
-        CellPhoneProvider cellPhoneProvider = new CellPhoneProvider();
+        CellPhoneProvider cellPhoneProvider = new CellPhoneProvider(List.of(new SamsungCellPhoneFactory(), new AppleCellPhoneFactory()));
 
         CellPhone cellPhone = cellPhoneProvider.createCellPhone(cellPhoneType);
 
