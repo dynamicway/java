cd94e55709f16b8855ed631d5aceceb268f224a7
bee <rkddbtjd123@gmail.com>
2022-09-19 15:38:15 +0900

    feat: Add SamsungCellPhone objects

---
+++ design-pattern/src/main/java/me/java/designpattern/creation/abstractfactory/phone/samsung/SamsungCellPhoneCase.java
@@ -0,0 +1,6 @@
+package me.java.designpattern.creation.abstractfactory.phone.samsung;
+
+import me.java.designpattern.creation.abstractfactory.phone.CellPhoneCase;
+
+class SamsungCellPhoneCase implements CellPhoneCase {
+}
---
+++ design-pattern/src/main/java/me/java/designpattern/creation/abstractfactory/phone/samsung/SamsungCellPhoneFactory.java
@@ -0,0 +1,15 @@
+package me.java.designpattern.creation.abstractfactory.phone.samsung;
+
+import me.java.designpattern.creation.abstractfactory.phone.CellPhone;
+import me.java.designpattern.creation.abstractfactory.phone.CellPhoneFactory;
+
+public class SamsungCellPhoneFactory implements CellPhoneFactory {
+
+    @Override
+    public CellPhone createCellPhone() {
+        SamsungCellPhoneDevice samsungCellPhoneDevice = new SamsungCellPhoneDevice();
+        SamsungCellPhoneCase samsungCellPhoneCase = new SamsungCellPhoneCase();
+        return new CellPhone(samsungCellPhoneCase, samsungCellPhoneDevice, CellPhone.Type.SAMSUNG);
+    }
+
+}
