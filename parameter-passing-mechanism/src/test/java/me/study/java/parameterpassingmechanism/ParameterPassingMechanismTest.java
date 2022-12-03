520c9a66035aa4e008c62765bcc8ee58810b88ec
bee <rkddbtjd123@gmail.com>
2022-12-03 23:38:07 +0900

    test: Add ParameterPassingMechanismTest::when_modifying_objects_then_original_objects_changed

--- parameter-passing-mechanism/src/test/java/me/study/java/parameterpassingmechanism/ParameterPassingMechanismTest.java
+++ parameter-passing-mechanism/src/test/java/me/study/java/parameterpassingmechanism/ParameterPassingMechanismTest.java
@@ -19,4 +19,33 @@ class ParameterPassingMechanismTest {
         originValue = 20;
     }
 
+    @Test
+    void when_modifying_objects_then_original_objects_changed() {
+        Person givenPerson = new Person("Origin");
+
+        changeName(givenPerson);
+
+        assertThat(givenPerson.getName()).isEqualTo("Changed");
+    }
+
+    private void changeName(final Person person) {
+        person.setName("Changed");
+    }
+
+    private static class Person {
+        private String name;
+
+        public Person(final String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(final String name) {
+            this.name = name;
+        }
+    }
+
 }
