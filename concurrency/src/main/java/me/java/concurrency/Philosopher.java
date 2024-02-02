c4d1f786d4feb39f46a196abb7632ad1b278aec6
bee <rkddbtjd123@gmail.com>
2024-02-02 14:06:46 +0900

    feat: Add Philosopher's eating time

--- concurrency/src/main/java/me/java/concurrency/Philosopher.java
+++ concurrency/src/main/java/me/java/concurrency/Philosopher.java
@@ -1,18 +1,29 @@
 package me.java.concurrency;
 
 class Philosopher {
-    private int forksCount;
+    private final int eatingSeconds;
+    private final int forksCount;
     private boolean eaten = false;
 
-    Philosopher(int forksCount) {
+    Philosopher(int eatingSeconds, int forksCount) {
+        this.eatingSeconds = eatingSeconds;
         this.forksCount = forksCount;
     }
 
     public void eatSpaghettiOnTable(Table table) {
         if (forksCount < 2)
             table.preemptFork();
-        eaten = true;
+        eat();
         table.addForks(forksCount);
+    }
+
+    private void eat() {
+        try {
+            Thread.sleep(eatingSeconds * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        eaten = true;
     }
 
     int getForksCount() {
--- concurrency/src/test/java/me/java/concurrency/PhilosopherTest.java
+++ concurrency/src/test/java/me/java/concurrency/PhilosopherTest.java
@@ -6,21 +6,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+
 class PhilosopherTest {
 
     @Test
     void take_the_fork_from_the_table_if_have_less_than_one_fork() {
         Table table = new Table(1);
-        Philosopher sut = new Philosopher(1);
+        Philosopher sut = getPhilosopherByForksCount(1);
 
         assertDoesNotThrow(() -> sut.eatSpaghettiOnTable(table));
         assertThat(sut.hasEaten()).isTrue();
     }
 
+    private Philosopher getPhilosopherByForksCount(int forksCount) {
+        return new Philosopher(1, forksCount);
+    }
+
     @Test
     void cannot_eat_if_do_not_have_enough_forks_and_fail_to_get_a_fork_from_the_table() {
         Table table = new Table(0);
-        Philosopher sut = new Philosopher(1);
+        Philosopher sut = getPhilosopherByForksCount(1);
 
         assertThatThrownBy(() -> sut.eatSpaghettiOnTable(table))
                 .hasMessageContaining("There is no forks.");
@@ -30,7 +35,7 @@ class PhilosopherTest {
     @Test
     void do_not_take_a_fork_from_the_table_if_you_have_enough_forks() {
         Table table = new Table(0);
-        Philosopher sut = new Philosopher(2);
+        Philosopher sut = getPhilosopherByForksCount(2);
 
         assertDoesNotThrow(() -> sut.eatSpaghettiOnTable(table));
         assertThat(sut.hasEaten()).isTrue();
@@ -39,13 +44,28 @@ class PhilosopherTest {
     @Test
     void put_all_forks_after_eating_if_have_two_or_more_forks() {
         Table table = new Table(0);
-        Philosopher sut = new Philosopher(2);
+        Philosopher sut = getPhilosopherByForksCount(2);
 
         sut.eatSpaghettiOnTable(table);
 
         assertThat(table.getForksCount()).isEqualTo(2);
         assertThat(sut.getForksCount()).isEqualTo(2);
         assertThat(sut.hasEaten()).isTrue();
+    }
+
+    @Test
+    void it_takes_time_to_eat() {
+        Table table = new Table(0);
+        int eatingSeconds = 1;
+        Philosopher sut = new Philosopher(eatingSeconds, 2);
+        long startTime = System.currentTimeMillis();
+
+        sut.eatSpaghettiOnTable(table);
+        long endTime = System.currentTimeMillis();
+
+        long timeTaken = endTime - startTime;
+        long eatingMilliSeconds = eatingSeconds * 1000L;
+        assertThat(eatingMilliSeconds).isLessThanOrEqualTo(timeTaken);
     }
 
 }
